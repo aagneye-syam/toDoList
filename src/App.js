37b812e4da8951fcf3da6c536b65dbe6ac6b8daf
aagneye-syam <saagneye2003@gmail.com>
2024-01-25 23:48:06 +0530

    onChange the id is chacked and the status is updated

--- src/App.js
+++ src/App.js
@@ -22,17 +22,36 @@ function App() {
           placeholder="🖊️ Add item..."
         />
         <i
-          onClick={() => setToDos([...toDos,{id : Date.now(), text : toDo, status : false} ])}
+          onClick={() =>
+            setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])
+          }
           className="fas fa-plus"
         ></i>
       </div>
       <div className="todos">
-        {toDos.map((value) => {
+        {toDos.map((obj) => {
           return (
             <div className="todo">
               <div className="left">
-                <input value={value.status} type="checkbox" name="" id="" />
-                <p>{value.text}</p>
+                <input
+                  onChange={(e) => {
+                    console.log(e.target.value);
+                    console.log(obj);
+                    setToDos(
+                      toDos.filter((obj2) => {
+                        if (obj2.id === obj.id) {
+                          obj2.status = true;
+                        }
+                        return obj2;
+                      })
+                    );
+                  }}
+                  value={obj.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <p>{obj.text}</p>
               </div>
               <div className="right">
                 <i className="fas fa-times"></i>
